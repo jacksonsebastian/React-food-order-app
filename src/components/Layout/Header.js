5f9ba1ef759fefcc7c0e39da41936d33cf324cab
Jackson <technologyj4ck@gmail.com>
2022-08-16 10:15:34 +0530

    day1 Adding Components folder

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+import mealsImage from '../../Assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = (props) => {
+  return (
+    <Fragment>
+        <header className={classes.header}>
+        <h1>React Meals</h1>
+        <button>Cart</button>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt='A table of delicious food'/>
+        </div>
+
+    </Fragment>
+    
+  );
+};
+
+export default Header
