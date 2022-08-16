c3c8c7eb8664bdb3899a55b641c576c89373fe57
Jackson <technologyj4ck@gmail.com>
2022-08-16 20:57:52 +0530

    day 12 .3 Adding cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,6 @@
 import React from "react";
-import classes from "./cart.module.css";
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes['cart-items']}>
@@ -9,7 +10,7 @@ const Cart = (props) => {
     </ul>
   );
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total</span>
@@ -19,7 +20,7 @@ const Cart = (props) => {
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
