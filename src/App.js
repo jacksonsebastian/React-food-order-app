101a8d20abaadae2212e9fb6b0e104a23852fab7
Jackson <technologyj4ck@gmail.com>
2022-08-17 20:54:21 +0530

    Day 12 cart reducer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { Fragment } from "react";
+import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./Store/CartProvider";
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const shownCartHandler = () => {
+setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
-    <Fragment>
-      <Cart/>
-      <Header/>
+    <CartProvider>
+      {cartIsShown && <Cart onCloseCart={hideCartHandler}/> }
+      <Header onShowCart = {shownCartHandler} />
       <main>
         <Meals/>
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
