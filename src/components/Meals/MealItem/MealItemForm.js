77900f2fe2fb9cd61458ea188fd6b35009091720
Jackson <technologyj4ck@gmail.com>
2022-08-18 11:07:46 +0530

    Day 13 using ref in cart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
+
+
 const MealItemForm = (props) => {
+  const [amountIsvalid, setAmountIsValid] = useState(true)
+  const amountInputRef = useRef();
+
+  const sumbmitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber);
+  };
+  
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={sumbmitHandler}>
       <Input
+      ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount",
@@ -12,10 +35,11 @@ const MealItemForm = (props) => {
           min: "1",
           max: "5",
           step: "1",
-          defaultValue: "1"
+          defaultValue: "1",
         }}
       />
       <button>+ Add</button>
+      {!amountIsvalid && <p>Enter the valid amount (1 - 5).</p>}
     </form>
   );
 };
